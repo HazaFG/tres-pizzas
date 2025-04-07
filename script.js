74f2940ce51f276b2141c8b4cfa1353d9f2d877b
HazaFG <hazaelfg@gmail.com>
2025-04-07 11:07:02 -0700

    Pizzas pintadas

---
+++ script.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(1230, 500);
+}
+  
+function draw() {
+    background(255);
+
+    // dibuja los tres circulos xd
+    fill(255, 204, 0); 
+    stroke(150, 75, 0); 
+    strokeWeight(4);
+
+    ellipse(205, 250, 300, 300);
+    ellipse(615, 250, 300, 300);
+    ellipse(1025, 250, 300, 300);
+}
